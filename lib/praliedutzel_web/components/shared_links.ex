ef51ec3c0d4587b6e74f8909ff12b8a407a26099
praliedutzel <praliedutzel@gmail.com>
2024-07-23 16:06:04 -0500

    Add portfolio links for Figma and GitHub

--- lib/praliedutzel_web/components/shared_links.ex
+++ lib/praliedutzel_web/components/shared_links.ex
@@ -8,7 +8,9 @@ defmodule PraliedutzelWeb.SharedLinks do
 
   def resume_link, do: ~p"/pdfs/pralie-dutzel-resume-senior-designer-developer.pdf"
 
-  def portfolio_figma_link, do: nil
+  def portfolio_figma_link,
+    do:
+      "https://www.figma.com/design/vmCb7DuY70KQEMOoMHsmMR/Portfolio-Site?node-id=296-3792&t=g0BRhBDOCL0SDbgN-1"
 
-  def portfolio_github_link, do: nil
+  def portfolio_github_link, do: "https://github.com/praliedutzel/praliedutzel"
 end
